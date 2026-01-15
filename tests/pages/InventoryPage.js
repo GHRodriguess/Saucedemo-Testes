b06083252865c453b2b7351ec66096eb26e8fc63
GHRodriguess <gabri.henri20088@gmail.com>
2026-01-15 10:47:59 -0300

    testes da tela inventory e details concluidos

--- tests/pages/InventoryPage.js
+++ tests/pages/InventoryPage.js
@@ -1,8 +1,152 @@
+import { expect } from '@playwright/test';
+
 export class InventoryPage {
     constructor(page) {
         this.page = page;
-        
+        this.inventoryList = this.page.locator('[data-test="inventory-item"]');
+        this.cart = this.page.locator('[data-test="shopping-cart-link"]')
+        this.sortContainer = this.page.locator('[data-test="product-sort-container"]')
     }
 
+    async getProducts() {
+        const count = await this.inventoryList.count();
+        const products = [];
+        for (let i = 0; i < count; i++) {
+            const product = this.inventoryList.nth(i); 
+            products.push(new Product(product))
+        }
+        return products;
+    }
 
+    async getCartBadgeValue() {
+        const badge = this.cart.locator('[data-test="shopping-cart-badge"]');
+
+        if (await badge.count() === 0) {
+            return 0;
+        }
+
+        return Number(await badge.textContent());
+    }
+
+    async sortProducts(sortType) {
+        this.sortContainer.selectOption(sortType);
+    }
+
+    async getProductNames() {
+        const products = await this.getProducts();
+        const names = [];
+        for (const product of products) {
+            names.push(await product.name.textContent());
+        }
+        return names;
+    }
+
+    async getProductPrices() {
+        const products = await this.getProducts();
+        const prices = [];
+        for (const product of products) {
+            const priceText = await product.price.textContent();
+            const priceValue = parseFloat(priceText.replace('$', ''));
+            prices.push(priceValue);
+        }
+        return prices;
+    }
+
+    async verifySortProducts(sortType) {
+        const products = await this.getProducts();
+        let values = [];
+        for (const product of products) {
+            if (sortType === "az" || sortType === "za") {
+                values.push(await product.name.textContent());                
+            } else {
+                const priceText = await product.price.textContent();
+                const priceValue = Number(priceText.replace('$', ''));
+                values.push(priceValue);               
+            }
+        }
+
+        let sortedValues;
+
+        if (sortType === "az") {
+            sortedValues = [...values].sort((a, b) => a.localeCompare(b));
+        }
+
+        if (sortType === "za") {
+            sortedValues = [...values].sort((a, b) => b.localeCompare(a));
+        }
+
+        if (sortType === "lohi") {
+            sortedValues = [...values].sort((a, b) => a - b);
+        }
+
+        if (sortType === "hilo") {
+            sortedValues = [...values].sort((a, b) => b - a);
+        }
+        
+        return JSON.stringify(values) === JSON.stringify(sortedValues);
+    }
+
+}
+
+class BaseProduct {
+    constructor(root) {
+        this.root = root;        
+        this.name = root.locator('[data-test="inventory-item-name"]');
+        this.description = root.locator('[data-test="inventory-item-desc"]');
+        this.price = root.locator('[data-test="inventory-item-price"]');
+        this.addOrRemoveFromCartButton = root.locator('.btn_inventory');
+    }
+
+    async addToCart() {
+        if (await this.addOrRemoveFromCartButton.textContent() === 'Add to cart') {            
+            await this.addOrRemoveFromCartButton.click();
+        }
+    }
+
+    async removeFromCart() {
+        if (await this.addOrRemoveFromCartButton.textContent() === 'Remove') {            
+            await this.addOrRemoveFromCartButton.click();
+        }
+    }
+
+    async clickName() {
+        await this.name.click();
+    }
+
+    async clickImage() {
+        await this.image.click();
+    }    
+
+    async validatedCommonElements() {
+        const elements = [this.name, this.description, this.price, this.addOrRemoveFromCartButton];
+        for (const element of elements) {
+            await expect(element).toBeVisible();
+        } 
+        await expect(this.addOrRemoveFromCartButton).toContainText(/Add to cart|Remove/);
+    }
+}
+
+class Product extends BaseProduct {
+    constructor(product) {
+        super(product)        
+        this.image = this.root.locator('.inventory_item_img img');        
+    }
+
+    async isVisible() {
+        await this.validatedCommonElements();
+        await expect(this.image).toBeVisible();
+    }
+}
+
+export class ProductDetailsPage extends BaseProduct {
+    constructor(page) {
+        super(page)
+        this.image = this.root.locator('.inventory_details_img');
+        
+}
+
+    async isVisible() {
+        await this.validatedCommonElements();
+        await expect(this.image).toBeVisible();
+    }
 }
--- tests/specs/inventory.spec.js
+++ tests/specs/inventory.spec.js
@@ -1,14 +1,136 @@
 import {test, expect} from '@playwright/test';
-import {InventoryPage} from '../pages/InventoryPage';
+import {InventoryPage, ProductDetailsPage } from '../pages/InventoryPage';
 
 test.describe('Inventory Page Tests', () => {
     test.beforeEach(async ({page}) => {
         await page.goto('https://www.saucedemo.com/inventory.html');
+        const inventoryTitle = page.locator('.title');
+
+        if (!(await inventoryTitle.isVisible())) {
+            throw new Error('Usuário não está logado. Rode o teste de "login com sucesso" para atualizar o "storageState.json".');
+        }
     })
 
     test('verifica se os produtos estao visiveis na pagina de inventario', async ({page}) => {
         const inventoryPage = new InventoryPage(page);
-
-
+        const products = await inventoryPage.getProducts();
+        for (const product of products) {
+            await product.isVisible();
+        }
     })
+
+    test('verifica a funcionalidade de adicionar e remover produtos do carrinho', async ({page}) => {
+        const inventoryPage = new InventoryPage(page);
+        const products = await inventoryPage.getProducts();
+        let cartBadgeValue = await inventoryPage.getCartBadgeValue();        
+        for (const product of products) {
+            await product.addToCart();
+            cartBadgeValue = await inventoryPage.getCartBadgeValue();            
+            expect(cartBadgeValue).toBeGreaterThan(0);         
+            await product.removeFromCart();
+            cartBadgeValue = await inventoryPage.getCartBadgeValue();
+            expect(cartBadgeValue).toBe(0);  
+        }
+    })
+
+    test('verifica se o clique nos produtos redireciona para a pagina de detalhes', async ({page}) => {
+        const inventoryPage = new InventoryPage(page);
+        const products = await inventoryPage.getProducts();
+        for (const product of products) {
+            await product.clickName();
+            await expect(page).toHaveURL(/inventory-item/);            
+            const productDetailsPage = new ProductDetailsPage(page.locator('[data-test="inventory-item"]'));
+            await productDetailsPage.isVisible();
+            await page.goBack();
+            
+            await product.clickImage();
+            await expect(page).toHaveURL(/inventory-item/);   
+            await productDetailsPage.isVisible();       
+            await page.goBack();    
+        }
+    }) 
+
+    const sortTypes = [
+        {
+            label: "Name (A to Z)", value: "az"
+        },
+        {
+            label: "Name (Z to A)", value: "za"
+        },
+        {
+            label: "Price (low to high)", value: "lohi"
+        },
+        {
+            label: "Price (high to low)", value: "hilo"
+        }
+    ]
+
+    for (const sort of sortTypes) {
+        test(`verifica a ordenação dos produtos por ${sort.label}`, async ({page}) => {
+            const inventoryPage = new InventoryPage(page);
+            await inventoryPage.sortProducts(sort.value);
+            const products = await inventoryPage.getProducts();
+            for (const product of products) {
+                await product.isVisible();
+            }
+
+            const isSorted = await inventoryPage.verifySortProducts(sort.value);
+            expect(isSorted).toBe(true)
+            
+        })
+    }
+})
+
+test.describe('Detail Page Tests', () => {
+    test.beforeEach(async ({page}) => {
+        await page.goto('https://www.saucedemo.com/inventory.html');
+        const inventoryTitle = page.locator('.title');
+
+        if (!(await inventoryTitle.isVisible())) {
+            throw new Error('Usuário não está logado. Rode o teste de "login com sucesso" para atualizar o "storageState.json".');
+        }
+    })
+
+    test('verifica se as informacoes dos produtos estao corretas', async ({page}) => {
+        const inventoryPage = new InventoryPage(page);
+        const products = await inventoryPage.getProducts();
+        for (const product of products) {
+            const name = await product.name.textContent();
+            const description = await product.description.textContent();
+            const price = await product.price.textContent();
+            await product.clickImage();
+            await expect(page).toHaveURL(/inventory-item/); 
+            const productDetailsPage = new ProductDetailsPage(page.locator('[data-test="inventory-item"]'));
+            await productDetailsPage.isVisible();
+
+            const detailName = await productDetailsPage.name.textContent();
+            const detailDescription = await productDetailsPage.description.textContent();
+            const detailPrice = await productDetailsPage.price.textContent();
+
+            
+            expect(name).toBe(detailName);
+            expect(description).toBe(detailDescription);
+            expect(price).toBe(detailPrice);
+            await page.goBack();
+
+        }
+    })
+
+    test('verifica a funcionalidade de adicionar e remover produtos do carrinho na tela detalhes', async ({page}) => {
+        const inventoryPage = new InventoryPage(page);
+        const products = await inventoryPage.getProducts();
+        let cartBadgeValue = await inventoryPage.getCartBadgeValue();        
+        for (const product of products) {
+            await product.clickImage();
+            const productDetailsPage = new ProductDetailsPage(page.locator('[data-test="inventory-item"]'));
+            await productDetailsPage.addToCart();
+            cartBadgeValue = await inventoryPage.getCartBadgeValue();            
+            expect(cartBadgeValue).toBeGreaterThan(0);         
+            await productDetailsPage.removeFromCart();
+            cartBadgeValue = await inventoryPage.getCartBadgeValue();
+            expect(cartBadgeValue).toBe(0);  
+            await page.goBack();
+        }
+    })
+
 })
